3ad6fee91ffb9156e473507027e54ebb87bfd3d9
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-03-08 17:09:01 -0300

    subindo requisição com api e renderização dos nomes

--- pokedex/src/pages/HomePage/HomePage.jsx
+++ pokedex/src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,39 @@
-import { Button } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BASE_URL } from "../../constants/url";
+// import useRequestData from "../../hooks/useRequestData";
 
-const HomePage = () =>{
-    return (
-      <div >
-       <h1>Hello HomePage</h1>
-       <Button
-       variant="outlined"
-       color={"primary"}
-       >
-         Entrar
-       </Button>
-      </div>
-    );
+const HomePage = () => {
+  const [listPokemon, setListPokemon] = useState([])
+  useEffect(() => {
+    getPokemon()
+  }, [])
+  const getPokemon = () => {
+    axios.get(`${BASE_URL}`)
+      .then((response) => {
+        // console.log(response.data.results)
+        setListPokemon(response.data.results)
+        
+      })
+      .catch((error) => {
+        console.log(error.response)
+      })
   }
-  
-  export default HomePage;
+
+  return (
+    <div >
+      {listPokemon && listPokemon.map((pokemon) => {
+        return (
+          <div key={pokemon.url}>
+            <p>{pokemon.name}</p>
+            <img src={pokemon.sprites} alt={pokemon.name} />
+          </div>
+        )
+
+      })}
+
+    </div>
+  );
+}
+
+export default HomePage;
