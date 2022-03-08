76119084e5220cc5d028f700bf79a528b7407a27
Vitorduarte0 <viorsanta.cal.10@gmail.com>
2022-03-08 17:53:03 -0300

    subindo novo componente cardPokemon e requisição das imagens dos pokemons

--- pokedex/src/components/CardPokemon/CardPokemon.jsx
+++ pokedex/src/components/CardPokemon/CardPokemon.jsx
@@ -1,6 +1,29 @@
-const CardPokemon = () =>{
+import axios from "axios"
+import { useState } from "react"
+import { BASE_URL } from "../../constants/url"
+// import { CardContainer } from "./styled"
+
+const CardPokemon = ({name, url}) =>{
+    // console.log(name)
+    const [listaPhoto, setListaPhoto] = useState([])
+    const getPhoto = (name)=>{
+        axios.get(`${BASE_URL}/${name}`)
+        .then((response)=>{
+            console.log(response.data.sprites.front_default)
+            setListaPhoto(response.data.sprites.front_default)
+        })
+        .catch((error)=>{
+            console.log(error.response)
+        })
+    }
+    getPhoto(name)
     return (
-        <div></div>
+        <div>
+            <p>{name}</p>
+            <img src={listaPhoto} alt={name} />
+            <button>Adicionar a pokedex</button>
+            <button>Ver detalhes</button>
+        </div>
     )
 }
 
